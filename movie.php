b655cb064cabb5cc6a5fa8fd2219eb496c44cfc2
Hemanth-1354 <hemanthkumar_polisetti@srmap.edu.in>
2024-04-18 22:53:08 +0530

    Signin page update

--- movie.php
+++ movie.php
@@ -15,7 +15,7 @@ if (!empty($id)) {
     die('connect error(' . mysqli_connect_errno() . ')'
       . mysqli_connect_error());
   } else {
-    $sql = "INSERT INTO bookings(id,Movie_Name,No_of_tickets,Date_of_booking,message)
+    $sql = "INSERT INTO bookings(ViewerID,Movie_Name,No_of_tickets,Date_of_booking,message)
 values('$id','$mname','$name','$marks','$cgpa')";
     if ($conn->query($sql)) {
       echo "Booked Succesfully!!
